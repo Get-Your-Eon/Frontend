643a5ed868fd1a98ae67eca3f0c3a43040c5585c
JiYu <eunjun1018@gmail.com>
2025-10-22 17:55:13 +0900

    geolocation으로 현재위치 마커표시

--- src/pages/get-charge.jsx
+++ src/pages/get-charge.jsx
@@ -1,9 +1,48 @@
-import { useEffect, useRef } from 'react';
-import { Map } from "react-kakao-maps-sdk"
-// import useKakaoLoader from "./useKakaoLoader"
+import { Map, MapMarker } from "react-kakao-maps-sdk"
 import { Footer } from "@/widgets/layout";
+import { useEffect, useState } from "react";
 
 export function GetCharge() {
+
+  const [state, setState] = useState({
+    center: { 
+      lat: 33.450701, 
+      lng: 126.570667 
+    },
+    errMsg: null,
+    isLoading: true,
+  });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setState((prev) => ({
+            ...prev,
+            center: { 
+              lat: position.coords.latitude, // 위도
+              lng: position.coords.longitude, // 경도 
+            },
+            isLoading: false,
+          }));
+        },
+        (err) => {
+          setState((prev) => ({
+            ...prev,
+            errMsg: err.message,
+            isLoading: false,
+          }));
+        }
+      );
+    } else {
+      setState((prev) => ({
+        ...prev,
+        errMsg: "Geolocation is not supported by this browser.",
+        isLoading: false,
+      }));
+    }
+  }, []);
+
   return (
     <>
       <section className="relative block h-[13vh]">
@@ -11,21 +50,24 @@ export function GetCharge() {
         <div className="absolute top-0 h-full w-full bg-black/60 bg-cover bg-center" />
       </section>
 
-      <section className="p-4">
-        <Map // 지도를 표시할 Container
-      id="map"
-      center={{
-        // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
-      }}
-      style={{
-        // 지도의 크기
-        width: "100%",
-        height: "350px",
-      }}
-      level={3} // 지도의 확대 레벨
-    />
+      <section>
+        <Map
+          id="map"
+          center={state.center}
+          style={{
+            width: "100%",
+            height: "700px",
+          }}
+          level={3} // 지도의 확대 레벨
+        >
+          {!state.isLoading && (
+          <MapMarker position={state.center}>
+            {/* <div style={{ padding: "5px", color: "#000" }}>
+              {state.errMsg ? state.errMsg : "여기에 계신가요?!"}
+            </div> */}
+          </MapMarker>
+        )}
+        </Map>
       </section>
 
       <div className="bg-white">
